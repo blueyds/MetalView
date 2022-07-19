4b79589791bf88e61164ce6264df2932d69c50e0
Craig Nunemaker <craig@nunemakerllc.com>
2022-07-18 21:29:54 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MetalView
+
+A description of this package.
---
+++ Tests/MetalViewTests/MetalViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MetalView
+
+final class MetalViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MetalView().text, "Hello, World!")
+    }
+}
